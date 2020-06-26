333a5ae03ef782115424aa381ffcb02c0710171c
abelbcarvalho <abelbdecarvalho@gmail.com>
2020-06-25 21:13:34 -0300

    Desk Model Class Inserted

---
+++ src/com/bcarsoft/model/Desk.py
@@ -0,0 +1,19 @@
+class Desk:
+    name: str
+    comment: str
+    execute: str
+    path: str
+    icon: str
+    terminal: bool
+    typee: str
+    categories: list
+
+    def __init__(self):
+        self.name = ""
+        self.execute = ""
+        self.path = ""
+        self.icon = ""
+        self.terminal = False
+        self.typee = ""
+        self.categories = []
+
